b6d489a496a21b111ca99b1995f14abb00c51232
cbgfx <cbgfx@metz.space>
2023-04-23 14:42:25 -0400

    Footer update

--- src/app/components/Reusable_Components/Footer.js
+++ src/app/components/Reusable_Components/Footer.js
@@ -7,7 +7,6 @@ class Footer extends React.Component {
     return (
       <div>
       <div style={{position:"relative", bottom:"0", height: "50px", width: "100%", textAlign:"center", marginTop: "50px", marginBottom:"25px", padding: "10px",}}>
-      <small> We do use "Strictly Necessary Cookies", as defined by the GDPR. A session cookie will be placed in order to be able to keep you logged in throughout the experience.</small>
       <br />
         <font color="red">RedNimbus&#169;</font> {new Date().getFullYear()} GastroTech App v{constants.APPVERSION} BETA, All Rights Reserved.
         <div><a href="/privacy.html">Privacy Policy.</a></div>
